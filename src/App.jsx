daeb8d957f313b84c072b85ebed33b3f73ba30e3
TatianaSnisarenko <75484524+TatianaSnisarenko@users.noreply.github.com>
2024-11-17 18:43:32 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "modern-normalize/modern-normalize.css";
+import userData from "./userData.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+
+export default App;
